3c494dd08ba9b48420977db4f08af3d79d0cac56
k9lin <58714561+k9lin@users.noreply.github.com>
2019-12-22 12:00:09 -0800

    Update hello_from_micro1.py

--- src/objects/micro1/hello_from_micro1.py
+++ src/objects/micro1/hello_from_micro1.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
+'''module doc string'''
 class Microclass1:
     '''Class doc string'''
     msg = None
-    
     def __init__(self, msgIn):
         '''docstring for constructor.'''
         self.set_messge(msgIn)
-        
     def say_it(self):
         '''docstring for say_it.'''
-        print(Microclass1.__name__ + u" says: " + self.msg)
-    
+        print(self.__name__ + u" says: " + self.msg)
     def set_message(self, msgIn):
         '''docstring for set_message.'''
         self.msg = msgIn
