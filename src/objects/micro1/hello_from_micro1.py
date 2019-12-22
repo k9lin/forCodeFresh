1792adda35be56f58dc28be7586f5f46841a37d8
k9lin <58714561+k9lin@users.noreply.github.com>
2019-12-22 11:43:37 -0800

    documentation to keep pylint happy

--- src/objects/micro1/hello_from_micro1.py
+++ src/objects/micro1/hello_from_micro1.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+"""Documentation for this class"""
 class Microclass1:
     msg = None
+    """Constructor"""
     def __init__(self, msgIn):
         self.msg = msgIn
-
+    """say_it method"""
     def say_it(self):
         print(Microclass1.__name__ + u" says: " + self.msg)
