ffb45493ec9148a916fe3fed0254169963ea9f1d
k9lin <58714561+k9lin@users.noreply.github.com>
2019-12-22 11:17:33 -0800

    fix pylint . issues
    fix pylint issues

--- src/objects/micro1/helloFromMicro1.py
+++ src/objects/micro1/helloFromMicro1.py
@@ -1,7 +1,7 @@
 class MicroClass1:
-  msg = None
-  def __init__(self, msgIn):
-    self.msg = msgIn
-    
-  def sayIt():
-    print(MicroClass1.__name__ + " says: " + self.msg)
+    msg = None
+    def __init__(self, msgIn):
+        self.msg = msgIn
+
+    def sayIt():
+        print(MicroClass1.__name__ + u" says: " + self.msg)
